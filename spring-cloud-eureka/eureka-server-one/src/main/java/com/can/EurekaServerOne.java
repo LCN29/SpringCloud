5aacb92b749e0a822f9b457fe082bc2e351abe4a
LCN29 <1455476411@qq.com>
2019-10-03 16:03:53 +0800

    eureka服务端提交

---
+++ spring-cloud-eureka/eureka-server-one/src/main/java/com/can/EurekaServerOne.java
@@ -0,0 +1,15 @@
+package com.can;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerOne {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerOne.class, args);
+    }
+
+}
